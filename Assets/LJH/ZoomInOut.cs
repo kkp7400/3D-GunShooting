ee8103c1ff02eb584ba1f946b77fa1b3bc517f0a
wing7sing <80494197+wing7sing@users.noreply.github.com>
2021-06-27 03:50:52 +0900

    카메라 줌, 시점, 총알ui 추가
    그러함

--- Assets/LJH/ZoomInOut.cs
+++ Assets/LJH/ZoomInOut.cs
@@ -5,42 +5,70 @@ using UnityEngine;
 public class ZoomInOut : MonoBehaviour
 {
     [SerializeField]
-    public List<Transform> target = new List<Transform>();
+    private List<Transform> target = new List<Transform>();
     EnemySpawner es;
-    public List<GameObject> enemyList = new List<GameObject>();
+    //public List<GameObject> enemyList = new List<GameObject>();
     public float zoom;
     private Transform tr;
-    int num = 0;
-
+    private int num;
+    
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         tr = GetComponent<Transform>();
         es = FindObjectOfType<EnemySpawner>();
+        num = 0;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-       while(true)
+        for (int i = 0; i < es.enemyList.Count; i++)
         {
-            if (es.enemyList[num].GetComponent<Enemy>().isDead == false)
-            {
-                Vector3 TargetDist = tr.position - target[num].position;
-                TargetDist = Vector3.Normalize(TargetDist);
-                tr.position -= (TargetDist * 1 * zoom);
-                Quaternion q = Quaternion.LookRotation(target[num].position);
-                transform.LookAt(target[num].position);
-            }
-            if (es.enemyList[num].GetComponent<Enemy>().isDead == true)
-            {
-                num += 1;
-                Vector3 TargetDist = tr.position + target[num].position;
-                TargetDist = Vector3.Normalize(TargetDist);
-                tr.position += (TargetDist * 2 * zoom);
-                transform.LookAt(tr.position);
-            }
+            target[i].position = es.enemyList[i].GetComponent<Enemy>().transform.position;
+        }
+        int scase = 0;
+        if (es.enemyList[scase].GetComponent<Enemy>().isDead == false) CameraZoomIn(num);
+        if (es.enemyList[scase].GetComponent<Enemy>().isDead == true) CameraZoomOut(num);
+    }
+    void CameraZoomIn(int num)
+    {
+        Vector3 TargetDist = tr.position - (target[num].position + new Vector3(0, 1.5f, 0));
+        TargetDist = Vector3.Normalize(TargetDist);
+        Vector3 TargetMax = tr.position - (target[num].position + new Vector3(3, 1.5f, 3));
+        TargetMax = Vector3.Normalize(TargetMax);
+        Quaternion q = Quaternion.LookRotation(target[num].position + new Vector3(0, 1.5f, 0));
+        transform.LookAt(target[num].position + new Vector3(0, 1.5f, 0));
+        Vector3 distance = (TargetDist * 1f * zoom);
+        if (distance.x <= TargetMax.x && distance.z <= TargetMax.z )
+        {
+            tr.position -= distance;
         }
        
+            
+
+        //if (es.enemyList[num].GetComponent<Enemy>().isDead == false)
+        //{  
+        //}
+
+        //else if (es.enemyList[num].GetComponent<Enemy>().isDead == true)
+        //{
+        //    Vector3 TargetDist = tr.position - (target[num].position + new Vector3(0, +1.5f, 0));
+        //    TargetDist = Vector3.Normalize(TargetDist);
+        //    Quaternion q = Quaternion.LookRotation(target[num].position + new Vector3(0, +1.5f, 0));
+        //    transform.LookAt(target[num].position + new Vector3(0 + 1.5f, 0));
+        //    tr.position -= (TargetDist * -1 * zoom);
+        //    num++;
+        //}
+    }
+    void CameraZoomOut(int num)
+    {
+        Vector3 TargetDist = tr.position - (target[num].position + new Vector3(0, 1.5f, 0));
+        TargetDist = Vector3.Normalize(TargetDist);
+        Quaternion q = Quaternion.LookRotation(target[num].position + new Vector3(0, 1.5f, 0));
+        transform.LookAt(target[num].position + new Vector3(0, 1.5f, 0));
+        tr.position -= (TargetDist * -1 * zoom);
+        num++;
     }
 }
